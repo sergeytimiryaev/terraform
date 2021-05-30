58fed2f470cf02cc87c11ad68593f08475861119
sergeytimiryaev <sergey.timiryaev36@gmail.com>
2021-05-30 21:06:26 +0300

    update

--- fivexl.tf
+++ fivexl.tf
@@ -4,6 +4,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.ubuntu.id
+}
+
 resource "aws_instance" "ubuntu" {
   ami                    = "ami-05f7491af5eef733a"
   instance_type          = "t3.micro"
