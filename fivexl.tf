462904a8b8eb9ee87c8c7ef9ea9e3397be88c7a3
sergeytimiryaev <sergey.timiryaev36@gmail.com>
2021-06-07 11:38:12 +0300

    update

--- fivexl.tf
+++ fivexl.tf
@@ -8,8 +8,23 @@ resource "aws_eip" "my_static_ip" {
   instance = aws_instance.ubuntu.id
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "ubuntu" {
-  ami                    = "ami-05f7491af5eef733a"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.webserver.id]
   user_data              = <<EOF
